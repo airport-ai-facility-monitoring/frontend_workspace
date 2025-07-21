fcfebbea832fed812c790d173bf06a65cb7c4271
201924611 <hu349401@gmail.com>
2025-07-21 07:36:40 +0000

    appcss랑header수정

--- air_front/src/App.jsx
+++ air_front/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
+import Alert from './component/alert/Alert';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />} >
+        <Route path="/alert" element={<Alert/>}/>
         </Route>
       </Routes>
     </Router>
