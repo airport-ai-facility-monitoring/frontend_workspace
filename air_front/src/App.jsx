8b65a3d6bc4624f3f0ecd8d932ece58bbde9cb50
egoing <egoing@gmail.com>
2025-07-22 16:37:52 +0900

    app

--- air_front/src/App.jsx
+++ air_front/src/App.jsx
@@ -3,33 +3,13 @@ import mainRoutes from "./routes/mainRoutes";
 import adminRoutes from "./routes/adminRoutes";
 import Login from "./component/login/login";
 import SignUp from "./component/Signup/singup";
-import Login from "./component/login/login";
-import Anomaly from "./component/anomaly/Anomaly";
-import Anomalyreport from './component/Anomalyreport/anomalyreport';
-import AnomalyReportEdit from './component/anomalyreport/AnomalyreportEdit'
-import UserDetail from './component/admin/UserDetail'
-import UsersList from './component/admin/UsersList'
-
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
-        <Route path="/" element={<Layout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="/notifications/:id" element={<NotificationDetail />} />
-          <Route path="notifications/new" element={<NotificationWrite />} />
-          <Route path="/alert" element={<Alert />} />
-          <Route path="/dash" element={<Dashboard />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/anomaly" element={<Anomaly />} />
-          <Route path="/dashdetail" element={<DashDetail />} />
-          <Route path="/anomalyreport" element={<Anomalyreport />} />
-          <Route path="/anomalyreport/edit" element={<AnomalyReportEdit />} />
-        </Route>
+        {mainRoutes}
+        {adminRoutes}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
